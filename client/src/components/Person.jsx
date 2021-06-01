bed5d282ad63b7e7fc11a4858d795b2eb23a748d
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2021-06-01 16:54:22 -0400

    added bs carousel

--- client/src/components/Person.jsx
+++ client/src/components/Person.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import ExampleImage from '../images/lind.jpeg';
+import ExampleImage2 from '../images/drumset.jpeg';
+import ExampleImage3 from '../images/gemma-evans-_0t4BBaeKSo-unsplash.jpg';
 
 import LikeButton from './LikeButton';
 import SuperLikeButton from './SuperLikeButton';
@@ -16,13 +18,87 @@ export default function Person({
     return (
         <>
             <div className="card my-5 p-5">
-                <img
+                <div
+                    id="carouselExampleIndicators"
+                    class="carousel slide"
+                    // data-ride="carousel"
+                >
+                    <ol class="carousel-indicators">
+                        <li
+                            data-target="#carouselExampleIndicators"
+                            data-slide-to="0"
+                            class="active"
+                        ></li>
+                        <li
+                            data-target="#carouselExampleIndicators"
+                            data-slide-to="1"
+                        ></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img
+                                class="d-block w-100"
+                                src={ExampleImage}
+                                data-src="holder.js/900x400?theme=social"
+                                alt="900x400"
+                                data-holder-rendered="true"
+                                style={{ width: '900px;', height: '400px;' }}
+                            />
+                        </div>
+                        <div class="carousel-item">
+                            <img
+                                class="d-block w-100"
+                                src={ExampleImage2}
+                                data-src="holder.js/900x400?theme=industrial"
+                                alt="900x400"
+                                data-holder-rendered="true"
+                                style={{ width: '900px;', height: '400px;' }}
+                            />
+                        </div>
+                        <div class="carousel-item">
+                            <img
+                                class="d-block w-100"
+                                src={ExampleImage3}
+                                data-src="holder.js/900x400?theme=industrial"
+                                alt="900x400"
+                                data-holder-rendered="true"
+                                style={{ width: '900px;', height: '400px;' }}
+                            />
+                        </div>
+                    </div>
+                    <a
+                        class="carousel-control-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="prev"
+                    >
+                        <span
+                            class="carousel-control-prev-icon"
+                            aria-hidden="true"
+                        ></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a
+                        class="carousel-control-next"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="next"
+                    >
+                        <span
+                            class="carousel-control-next-icon"
+                            aria-hidden="true"
+                        ></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+                {/* <img
                     src={ExampleImage}
                     className="card-img-top mx-auto"
                     alt="Example user"
                     // style={{ width: '18rem' }}
                     // style={{ objectFit: 'contain' }}
-                />
+                /> */}
                 <div className="card-body">
                     <div className="d-flex justify-content-around">
                         <p className="card-text">
